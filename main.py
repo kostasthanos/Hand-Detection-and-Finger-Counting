fc9639c281c50ddbc8f2e087f8a100a9879f45eb
Konstantinos Thanos <59958024+kostasthanos@users.noreply.github.com>
2021-01-09 17:21:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ font = cv2.FONT_HERSHEY_PLAIN
 text_color = (255,255,255)
 
 # Video Capture
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # Define the Region Of Interest (ROI) window 
 # Only inside this window the results will be visible
-top_left = (245,150)
-bottom_right = (580,395)
+top_left = (245,50)
+bottom_right = (580,295)
 
 while True:
     _, frame = cap.read()
@@ -95,64 +95,64 @@ while True:
         # --Contour Polygon--
         contour_poly = cv2.approxPolyDP(max_contour, 0.01*cv2.arcLength(max_contour,True), True)
         # Draw contour polygon (white color)
-# ! #   cv2.fillPoly(test_window, [max_contour], text_color) 
+        #cv2.fillPoly(test_window, [max_contour], text_color) 
 
         hull = cv2.convexHull(contour_poly, returnPoints = False) # --Hull--
-        if len(hull) > 3:
-            # Find Defect points
-            defects = cv2.convexityDefects(contour_poly, hull)
-            if type(defects) != type(None):
-                count = 0
-                points = []
-                for i in range(defects.shape[0]): # Len of arrays
-                    start_index, end_index, far_pt_index, fix_dept = defects[i][0]
-                    start_pts = tuple(contour_poly[start_index][0])
-                    end_pts = tuple(contour_poly[end_index][0])
-                    mid_pts = (int((start_pts[0]+end_pts[0])/2), int((start_pts[1]+end_pts[1])/2))
-                    far_pts = tuple(contour_poly[far_pt_index][0])
-                    
-                    points.append(mid_pts)
-                    # Draw circle (yellow color in the mid_pts defect points)
-# ! #               cv2.circle(test_window, mid_pts, 2, (0,255,255), 2) 
-                    
-                    #--Start Points-- (yellow color)
-# ! #               cv2.circle(test_window, start_pts, 2, (0,255,255), 2)
-                    
-                    #--End Points-- (black color)
-# ! #               cv2.circle(test_window, end_pts, 2, (0), 2)
-                    
-                    #--Far Points-- (white color)
-# ! #               cv2.circle(test_window, far_pts, 2, text_color, 2)
 
-                    # Connect points with lines
-# ! #               cv2.line(test_window, start_pts, end_pts, (200,200,255), 2) # Connect with line start_pts and end_pts
-# ! #               cv2.line(test_window, start_pts, far_pts, (200,200,255), 2) # Connect with line start_pts and far_pts
-# ! #               cv2.line(test_window, end_pts, far_pts, (20,20,25), 2)      # Connect with line end_pts and far_pts
+        # Find Defect points
+        defects = cv2.convexityDefects(contour_poly, hull)
+        if defects is not None:
+            count = 0
+            points = []
+            for i in range(defects.shape[0]): # Len of arrays
+                start_index, end_index, far_pt_index, fix_dept = defects[i][0]
+                start_pts = tuple(contour_poly[start_index][0])
+                end_pts = tuple(contour_poly[end_index][0])
+                mid_pts = (int((start_pts[0]+end_pts[0])/2), int((start_pts[1]+end_pts[1])/2))
+                far_pts = tuple(contour_poly[far_pt_index][0])
+                
+                points.append(mid_pts)
+                # Draw circle (yellow color in the mid_pts defect points)
+# ! #                cv2.circle(test_window, mid_pts, 2, (0,255,255), 2) 
+                
+                #--Start Points-- (yellow color)
+# ! #                 cv2.circle(test_window, start_pts, 2, (0,255,255), 2)
+                
+                #--End Points-- (black color)
+# ! #                 cv2.circle(test_window, end_pts, 2, (0), 2)
+                
+                #--Far Points-- (white color)
+# ! #                 cv2.circle(test_window, far_pts, 2, text_color, 2)
 
-                    # --Calculate distances--
-                    # If p1 = (x1, y1) and p2 = (x2, y2) the the distance between them is
-                    # Dist : sqrt[(x2-x1)^2 + (y2-y1)^2]
-                    
-                    # Distance between the start and the end defect point
-                    a = np.sqrt((end_pts[0] - start_pts[0])**2 + (end_pts[1] - start_pts[1])**2)
-                    # Distance between the farthest point and the start point
-                    b = math.sqrt((far_pts[0] - start_pts[0])**2 + (far_pts[1] - start_pts[1])**2)
-                    # Distance between the farthest point and the end point
-                    c = math.sqrt((end_pts[0] - far_pts[0])**2 + (end_pts[1] - far_pts[1])**2)
-                    
-                    angle = math.acos((b**2 + c**2 - a**2) / (2*b*c))  # Find each angle
+                # Connect points with lines
+                cv2.line(test_window, start_pts, end_pts, (0,255,0), 2)   # Connect with line start_pts and end_pts
+                cv2.line(test_window, start_pts, far_pts, (0,255,255), 2) # Connect with line start_pts and far_pts
+                cv2.line(test_window, end_pts, far_pts, text_color, 2)    # Connect with line end_pts and far_pts
 
-                    # If angle > 90 then the farthest point is "outside the area of fingers"
-                    if angle <= 90:  
-                        count += 1
-                        cv2.circle(test_window, far_pts, 3, (0,255,255), -1)
-                        frame[0:40, w-40:w] = (0)
-                        for c in range(5):
-                            if count == c:
-                                cv2.putText(frame, str(count+1), (w-35,30), font, 2, text_color, 2)
-                    if len(points) <= 1 :
-                        frame[0:40, w-40:w] = (0)
-                        cv2.putText(frame, "1", (w-35,30), font, 2, text_color, 2)
+                # --Calculate distances--
+                # If p1 = (x1, y1) and p2 = (x2, y2) the the distance between them is
+                # Dist : sqrt[(x2-x1)^2 + (y2-y1)^2]
+                
+                # Distance between the start and the end defect point
+                a = np.sqrt((end_pts[0] - start_pts[0])**2 + (end_pts[1] - start_pts[1])**2)
+                # Distance between the farthest point and the start point
+                b = math.sqrt((far_pts[0] - start_pts[0])**2 + (far_pts[1] - start_pts[1])**2)
+                # Distance between the farthest point and the end point
+                c = math.sqrt((end_pts[0] - far_pts[0])**2 + (end_pts[1] - far_pts[1])**2)
+                
+                angle = math.acos((b**2 + c**2 - a**2) / (2*b*c))  # Find each angle
+
+                # If angle > 90 then the farthest point is "outside the area of fingers"
+                if angle <= np.pi/2:  
+                    count += 1
+# ! #                    cv2.circle(test_window, far_pts, 3, (0,255,255), -1)
+                    frame[0:40, w-40:w] = (0)
+                    for c in range(5):
+                        if count == c:
+                            cv2.putText(frame, str(count+1), (w-35,30), font, 2, text_color, 2)
+                if len(points) <= 1 :
+                    frame[0:40, w-40:w] = (0)
+                    cv2.putText(frame, "1", (w-35,30), font, 2, text_color, 2)
 
   
     cv2.imshow("Frame", frame)
@@ -160,6 +160,8 @@ while True:
     key = cv2.waitKey(25)
     if key==27: # Press ESC to exit
         break
+    elif key==ord('q'):
+        cv2.imwrite('imgs/img.png', test_window)
 
 cap.release()
 cv2.destroyAllWindows()
